eea999b2d02f5c3114787c061fd792a629f5433a
alonbirnberg18-star <alonbirnberg18@gmail.com>
2025-08-09 11:39:29 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,28 @@
+JavaScript
 
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ProfilePage from './pages/ProfilePage';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
