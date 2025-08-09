79be84d387c0874feeedb6750bb3f596fc8c0f6c
alonbirnberg18-star <alonbirnberg18@gmail.com>
2025-08-09 11:36:36 +0300

    Update authRoutes.js

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,1 +1,10 @@
+JavaScript
 
+const express = require('express');
+const { register, login } = require('../controllers/authController');
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+
+module.exports = router;
